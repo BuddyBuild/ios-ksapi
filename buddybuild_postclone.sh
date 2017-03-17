db9f8e4e2fedc1b846573c961621306fc1655624
Romain Pouclet <romain.pouclet@gmail.com>
2017-03-17 11:35:45 -0700

    Update sqlite

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -3,5 +3,6 @@ bin/bootstrap
 brew update
 brew install sqlite3 --force
 gem install bundler
-gem install sqlite3 -v '1.3.13'
+gem install sqlite3 -- --with-sqlite3-dir=/opt/local
+# gem install sqlite3 -v '1.3.13'
 bundle install
