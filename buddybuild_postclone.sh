eb948fcaa7563991a2e90155cd0f84afe1a4a13a
Romain Pouclet <romain.pouclet@gmail.com>
2017-03-20 09:21:50 -0400

    Update postclone script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -2,8 +2,8 @@
 bin/bootstrap
 
 #
-echo "SQLITE="
-which -a sqlite3
+# echo "SQLITE="
+# which -a sqlite3
 
 echo "Ruby version"
 which ruby
@@ -12,19 +12,23 @@ ruby -v
 # Install sqlite
 brew update
 brew install rbenv
-brew install sqlite3
 
 rbenv init
 
 rbenv install 2.4.0
+rbenv local 2.4.0
 
 echo "Ruby version"
 which ruby
 ruby -v
 
+# brew install sqlite3
+
 # Update bundler
 gem install bundler
 
-gem install sqlite3  -v '1.3.13' -- --with-sqlite3-dir=/usr/local/opt/sqlite
+rbenv rehash
+
+# gem install sqlite3  -v '1.3.13' -- --with-sqlite3-dir=/usr/local/opt/sqlite
 # gem install sqlite3 -v '1.3.13'
 bundle install
